e10ae16e50dd65d47e6e6f303f0d4cf8ac31015d
janchan333 <sebajakubiak67@gmail.com>
2026-01-28 23:23:46 +0100

    Add random number to sum and display results
    Initialize sum and add a random number to it, displaying results.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// Początkowa wartość
+let suma = 0;
+
+// Losowanie liczby od 0 do 10000
+let losowaLiczba = Math.floor(Math.random() * 10001);
+
+// Dodanie wylosowanej liczby do sumy
+suma += losowaLiczba;
+
+// Wyświetlenie wyników
+console.log("Wylosowana liczba:", losowaLiczba);
+console.log("Aktualna wartość sumy:", suma);
 
